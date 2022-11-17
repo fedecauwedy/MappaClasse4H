836862b483b0af25bfe1c0f1dd2138658bd99451
fedecauwedy <federico.cau@studenti.ittsrimini.edu.it>
2022-11-17 10:39:29 +0100

    Prima versione del codice

---
+++ cau.federico.4h.MappaClasse/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cau.federico._4h.MappaClasse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn1.Content = "Pompa";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn2.Content = "Salvi";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn3.Content = "Lombardi";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn4.Content = "Cau";
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn5.Content = "Capanna";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn6.Content = "islamowski";
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn7.Content = "Zang";
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn8.Content = "Morelli";
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn9.Content = "Casadei";
+        }
+
+        private void btn10_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn10.Content = "Ghinelli";
+        }
+
+        private void btn11_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn11.Content = "Capicchioni";
+        }
+
+        private void btn12_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn12.Content = "Burioli";
+        }
+
+        private void btn13_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn13.Content = "Basilico";
+        }
+
+        private void btn14_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn14.Content = "Cancellieri";
+        }
+
+        private void btn15_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn15.Content = "Rossini";
+        }
+
+        private void btn16_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn16.Content = "Leo";
+        }
+
+        private void btn17_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn17.Content = "Dervishi";
+        }
+
+
+        private void btn18_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("in questo banco è seduto: ");
+            btn18.Content = "Conti-Sartini";
+        }
+
+    }
+}
